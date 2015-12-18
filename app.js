f2f51896cc7326d0ede5f9bf8c96ff4fdf2cdf20
Madhu <madhuvanmahesh@gmail.com>
2015-12-18 17:19:26 +0530

    Update variable marker with prefix self.

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 
- // ---- Knockout MVVM pattern -----
+ // ---- Knockout MVVM pattern-----
 var map;
   
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-  //call function MapViewModel() here
-  ko.applyBindings(new MapViewModel());
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 8,
+		center: {lat: -34.397, lng: 150.644}
+	});
+	//call function MapViewModel() here
+	ko.applyBindings(new MapViewModel());
 }
 
+// ViewModel
 function MapViewModel(){
 	
 	var self = this;
-	
-	//ViewModel gets the data from the submit button and stores it in an observable object address
+	//ViewModel gets the data from the submit button and stores it in the observable variable address
 	self.address = ko.observable("sydney, NSW");
 	// Now that we have the address, we can use geocoder to get the location and display marker
 	self.geocoder = new google.maps.Geocoder();
 	self.displayLocMarker = function(){
-		  self.geocoder.geocode({'address': self.address()}, function(results, status) {
+			self.geocoder.geocode({'address': self.address()}, function(results, status) {
 			if (status === google.maps.GeocoderStatus.OK) {
 			map.setCenter(results[0].geometry.location);
-			var marker = new google.maps.Marker({
+			 self.marker = new google.maps.Marker({
 				map: map,
 				position: results[0].geometry.location
 			});
