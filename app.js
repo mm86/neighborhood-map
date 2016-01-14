dee2bbb4ce54c33735c248a41a51f97297a7d47e
Madhu <madhuvanmahesh@gmail.com>
2016-01-13 21:36:00 -0800

    Add function to display list of locations and corresponding markers
    Added  a function to app.js that displays list of locations and corresponding markers. Added a foreach binding in index.html to display the location names.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // ---- Knockout MVVM pattern-----
 var map;
-var infowindow;
-var geocoder;
+
 //function to display initial map
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -13,9 +12,17 @@ function initMap() {
 	});
 };
 //Start of MODEL in the MVVM pattern
+function Location(name,geometric_location){
 
-//End of Model
-	// ViewModel - handles all the interactions between the view and the model
+	var self = this;
+	self.name = name;
+	self.geometric_location = geometric_location;
+
+
+};
+//End of MODEL
+
+// ViewModel - handles all the interactions between the view and the model
 function MapViewModel() {
 	var self = this;
 	// ViewModel gets the data from the submit button and stores it in the observable variable address
@@ -23,11 +30,64 @@ function MapViewModel() {
 	self.address = ko.observable("sydney, NSW");
 	// Now that we have the address, we can use geocoder to get the location and display marker
 	// Define and use google maps objects here
-	geocoder = new google.maps.Geocoder();
+	self.geocoder = new google.maps.Geocoder();
 	// Create other functions to communicate with the Model, Observables, and Google's Maps (this can be thought of as a View)
-	self.displayLocMarker = function() {
-		return displayMarkers(self.address);
+	// array that holds a list of location objects, make this an observable array.why observable? cause the list 
+	// is dynamic and changes for every address input.
+   
+	self.locationListArray = ko.observableArray(); 
+	self.listOfLocations = function() {
+
+		self.geocoder.geocode({'address': self.address()}, function(results, status) {
+
+    	if (status === google.maps.GeocoderStatus.OK) {
+      		map = new google.maps.Map(document.getElementById('map'), {
+      		center: results[0].geometry.location,
+      		zoom: 15
+    	});
+    	var service = new google.maps.places.PlacesService(map);
+    	service.nearbySearch({
+    		location: results[0].geometry.location,
+    		radius: 2000,
+    		types: ['aquarium','campground','zoo','amusement_park','park','museum']
+  		}, self.processResults); //callback function will be called back (or executed) inside the nearbySearch function
+    	} else {
+      		alert('Geocode was not successful for the following reason: ' + status);
+    	}
+	});
+
+		
+
 	}
+
+	self.processResults = function(results, status) {
+  		if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    		return;
+  		} else {
+    		self.createMarkers(results);
+        }
+	};
+
+	self.createMarkers = function(places) {
+  
+  		var bounds = new google.maps.LatLngBounds();
+  		
+  		if(self.locationListArray().length !== 0){
+  			self.locationListArray().length = 0;
+  		}
+  		for (var i = 0, place; place = places[i]; i++) {
+    		var marker = new google.maps.Marker({
+      			map: map,
+	  			title: place.name,
+      			position: place.geometry.location
+    		});
+    	 
+        //locationListArray is an array that is a collection of objects. each object has a title and position property.
+		self.locationListArray.push(new Location(marker.title,marker.position));    
+    	bounds.extend(place.geometry.location);	
+		}
+ 		map.fitBounds(bounds);
+	};
 };
 
 //This function is google maps API's callback function
