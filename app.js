e304db4741c7a0401aea66b4bc013d20dce0f863
Madhu <madhuvanmahesh@gmail.com>
2016-01-15 12:07:16 -0800

    Add yelp oauth signature

--- app.js
+++ app.js
@@ -109,8 +109,53 @@ function MapViewModel() {
   };
 
   self.displayYelpDetails = function(){
-   
-      console.log("hi");
+ /**
+ * Generates a random number and returns it as a string for OAuthentication
+ * @return {string}
+ */
+function nonce_generate() {
+  return (Math.floor(Math.random() * 1e12).toString());
+}
+var yelp_url = 'http://api.yelp.com/v2/search';
+
+    var parameters = {
+      
+      oauth_consumer_key: 'U3i6g2plvtRlHuwvFO9VAA',
+      oauth_token: 'NU1k3_HCN2gg1rlHqBa347qu-ulDLFXx',
+      oauth_nonce: nonce_generate(),
+      oauth_timestamp: Math.floor(Date.now()/1000),
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_version : '1.0',
+      callback: 'cb', // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+      location: 'chicago',
+    term: 'food',
+    limit: 1
+      
+    };
+
+    var consumer_secret = 'Hge_8nzOiPYng41v4EkjEJNTe7I';
+    var token_secret = 'iP1-9Hmy1kY8LtgZ2OpSRsE0kYk';
+    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, consumer_secret, token_secret);
+    parameters.oauth_signature = encodedSignature;
+
+    var settings = {
+      url: yelp_url,
+      data: parameters,
+      cache: true,   // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", 
+      //invalidating our oauth-signature
+      dataType: 'jsonp',
+      jsonpCallback: 'cb',
+      success: function(results) {
+      // Do stuff with results
+      console.log("Yay");
+      },
+      fail: function(xhr, status, error) {
+      console.log("An AJAX error occured: " + status + "\nError: " + error + "\nError detail: " + xhr.responseText);
+      }
+    };
+    // Send AJAX query via jQuery library.
+    $.ajax(settings);
+  
   };
 
 
