2ad65f4fecd72817dd0da234029632cb0a3476b0
Madhu <madhuvanmahesh@gmail.com>
2016-01-31 22:28:46 -0800

    Get weather API data and implement marker animation functionality

--- app.js
+++ app.js
@@ -6,8 +6,7 @@
  */
 'use strict';
 var map;
-var count = 0;
-var infoWindowList = [];
+
 /**
 * @function initMap 
   @description Displays the initial map on application load
@@ -70,19 +69,35 @@ function Location(name, lat, lng, phone, img_url, rating, rating_img, addr1,addr
   
   //create a marker object for each location
   //READ MORE: You are creating a new marker object for every instance of the Location class. Is this approach ok?
-  var marker;
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(self.lat, self.lng),
-    map: map,
-    icon: image,
-    title: self.name
-  });
+        var marker;
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(self.lat, self.lng),
+          map: map,
+          icon: image,
+          title: self.name
+        });
   
 
   self.marker = marker;
+  self.marker.addListener("click",function(){self.toggleBounce()});
+  self.toggleBounce = function(){
+     var infowindow = new google.maps.InfoWindow;
+      console.log("hi");
+      /* Set timeout animation */
+      self.marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){ self.marker.setAnimation(null); }, 1400);
+      //self.infocontent = '<img src="http://openweathermap.org/img/w/'+self.weatherIcon+'.png">';   
+      /* Open info window and set its content */
+      
+      infowindow.setContent("hi");
+      infowindow.open(map, self.marker)
+      //setTimeout(function() {self.infowindow.open(null);}, 750);
+     
+  }
+
   self.typeVisible = ko.observable(false);
   self.nameVisible = ko.observable(true);
-
+ 
   self.showTypeLink = function() {
       self.typeVisible(!self.typeVisible());
       self.nameVisible(!self.nameVisible());
@@ -94,30 +109,7 @@ function Location(name, lat, lng, phone, img_url, rating, rating_img, addr1,addr
       self.nameVisible(!self.nameVisible());
     };
   
-  self.infowindow = new google.maps.InfoWindow;
-  
-    self.marker.addListener('click', function(){
-
-      /* Set timeout animation */
-      self.marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){ self.marker.setAnimation(null); }, 1400);
-      //contentString = '<h4>' + place.name + '</h4>\n<p>' + place.street + '</p>\n<p>' + place.city + '</p><a href= ' + place.url + '>' + place.url + '</a>';   
-      /* Open info window and set its content */
-
-      self.infowindow.setContent(self.marker.title);
-      self.infowindow.open(map, self.marker)
-      //setTimeout(function() {self.infowindow.open(null);}, 750);
-      infoWindowList.push(self.infowindow);
-    //close previously open infowindow
-    if (count > 0) {
-      infoWindowList[count - 1].close();
-    }
-    count = count + 1;
-    })
-
 }
-
-
 
 /**
  * @class MapViewModel
@@ -128,11 +120,11 @@ function MapViewModel() {
   self.address = ko.observable("sydney, NSW");
   self.query = ko.observable('');
 
-  self.test = ko.observableArray();
+  self.count = 0;
+  self.infoWindowList = [];
   self.geocoder = new google.maps.Geocoder();
-  
-
   self.locationListArray = ko.observableArray();
+  
   
 /**
  * @function listOfLocations
@@ -141,13 +133,15 @@ function MapViewModel() {
   self.listOfLocations = function() {
     
     if (self.locationListArray().length !== 0) {//set the locationListArray to empty for every new address search.
+
       self.locationListArray().length = 0;
     }
     
     self.getYelpData(self.address()); //call Yelp API for retrieving list of locations and their information for further display
+    
   };
 
-
+  
 /**
  * @function animateMarkers
  * @description For every list item clicked on the display, corresponding marker is bounced along with an infowindow 
@@ -164,13 +158,54 @@ function MapViewModel() {
         break;
       }
     }
+     
+    self.locationListArray()[index].marker.addListener("click",self.markerBounce(index));
 
-    google.maps.event.trigger(self.locationListArray()[index].marker, 'click');
+  };
+  
 
-   
+  self.markerBounce = function(index){
+
+     
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?lat='+self.locationListArray()[index].lat+'&lon='+self.locationListArray()[index].lng+'&appid=44db6a862fba0b067b1930da0d769e98';
+    
+    var settings = {
+      url: url, 
+      dataType: 'jsonp',
+      success: function(results) {
+       
+       self.weatherIcon = results.weather[0].icon;
+       self.infowindow = new google.maps.InfoWindow;
+  
+      /* Set timeout animation */
+      self.locationListArray()[index].marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){ self.locationListArray()[index].marker.setAnimation(null); }, 1400);
+      self.infocontent = '<img src="http://openweathermap.org/img/w/'+self.weatherIcon+'.png">';   
+      /* Open info window and set its content */
+      
+      self.infowindow.setContent(self.infocontent);
+      self.infowindow.open(map, self.locationListArray()[index].marker)
+      //setTimeout(function() {self.infowindow.open(null);}, 750);
+     self.infoWindowList.push(self.infowindow);
+    //close previously open infowindow
+    if (self.count > 0) {
+      self.infoWindowList[self.count - 1].close();
+    }
+    self.count = self.count + 1;
+       
+
+      },
+      fail: function(xhr, status, error) {
+        console.log("An AJAX error occured: " + status + "\nError: " + error + "\nError detail: " + xhr.responseText);
+      }
+    };
+    $.ajax(settings);
+  
+     
   };
 
-
+  
 
 /**
  * @function searchFilter
@@ -198,7 +233,7 @@ function MapViewModel() {
     }
   });
 
-
+ 
 /**
  * @function getYelpData
  * @description Given the address, retrieves information from Yelp API for further display and information
@@ -237,7 +272,8 @@ function MapViewModel() {
       dataType: 'jsonp',
       jsonpCallback: 'cb',
       success: function(results) {
-       
+        
+        
         map = new google.maps.Map(document.getElementById('map'), {
           center: {
             lat: results.region.center.latitude,
@@ -255,6 +291,7 @@ function MapViewModel() {
             results.businesses[i].rating_img_url_small, results.businesses[i].location
             .display_address[0], results.businesses[i].location.display_address[
               1], results.businesses[i].categories[0][0]));
+
           
         }
       },
@@ -264,7 +301,12 @@ function MapViewModel() {
     };
     $.ajax(settings);
   };
+
+  
+  
 }
+
+
 /**
  * @function startApp
  * @description callback function that is executed once google Maps asynchronous load is ready
